31f015950509c6c55290cfe8eb5de42dd9be23e7
alex-bucevschi <bucevschi.alexandru@gmail.com>
2017-01-07 14:27:17 +0200

    Update Madness_drivers.h

--- Ip Project/Madness_drivers.h
+++ Ip Project/Madness_drivers.h
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<cmath>
+#include<cstdlib>
+#include<time>
 #include <SFML/Graphics.hpp>
 #include "Map.h"
 #include "Car.h"
@@ -13,3 +15,4 @@ double Get(Vector2f pos, double angle);
 Vector2f Start();
 bool Move(Vector2f myPos, Vector2f newPos);
 Vector2f translate(Vector2f p, double angle, double distance)
+
