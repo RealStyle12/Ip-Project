ee1f3450dfc3e17c1a4c31cc8acd07f4db51f191
alex-bucevschi <bucevschi.alexandru@gmail.com>
2017-01-07 14:24:58 +0200

    Create Madness_drivers.h

---
+++ Ip Project/Madness_drivers.h
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+#include <SFML/Graphics.hpp>
+#include "Map.h"
+#include "Car.h"
+#include "Menu.h"
+#include "Box.h"
+
+using namespace std;
+using namespace sf;
+
+double Get(Vector2f pos, double angle);
+Vector2f Start();
+bool Move(Vector2f myPos, Vector2f newPos);
+Vector2f translate(Vector2f p, double angle, double distance)
